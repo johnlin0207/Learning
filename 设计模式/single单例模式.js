45bc7471246b3b581aa1d6b9fc2a9cf840a00789
linzhen <linzhen@haodf.com>
2022-02-24 16:22:43 +0800

    新增单例模式

---
+++ 设计模式/single单例模式.js
@@ -0,0 +1,20 @@
+// 利用闭包存储初始化后的实例
+var createSingle = (function(){
+    let instance = null;
+    return function(fn, ...args){
+        if(!instance){
+            instance = new fn(...args)
+        }
+        return instance
+    }
+})()
+
+// 处理只出一次弹窗
+class Popup {
+    constructor(...props){
+        console.log('将会出弹窗')
+        console.log(...props)
+    }
+}
+createSingle(Popup, '出了');
+createSingle(Popup, '再次调用出弹窗'); //不会再次出弹窗
