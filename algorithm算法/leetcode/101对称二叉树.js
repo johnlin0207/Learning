025e8e2c9d22ac258fbe4b8182efd4f5b7a2fe13
linzhen <linzhen02@cnpc.com.cn>
2022-09-01 15:31:31 +0800

    '新增二叉树算法'

--- algorithm算法/leetcode/101对称二叉树.js
+++ algorithm算法/leetcode/101对称二叉树.js
@@ -5,7 +5,43 @@ function TreeNode(val, left, right) {
 }
 
 /**
- * 层序迭代法，比较每一层的val是否对称，没有子节点的val值用undefined代替
+ * method1:递归法
+ * 对称二叉树思路跟翻转二叉树并不同，翻转二叉树只需要交换每一个节点的左右子节点即可，对称二叉树不能对一个节点的左右子节点作对比
+ * 对称二叉树是在交叉对比，即将左节点的左子元素跟右节点的右子元素对比，和将左节点的右子元素跟右节点的左子元素对比
+ * @param root
+ */
+function isSymmetric(root) {
+
+  function compare(root) {
+    if (!root) {
+      return false
+    }
+    return equal(root.left, root.right)
+  }
+
+  function equal(leftNode, rightNode) {
+    // 对于本轮对比的两个元素作比对
+    if (leftNode === null && rightNode === null) {
+      return true
+    }
+    if (leftNode === null || rightNode === null) {
+      return false
+    }
+    if (leftNode.val !== rightNode.val) {
+      return false
+    }
+    // 交叉对比：将左节点的左子元素跟右节点的右子元素对比，将左节点的右子元素跟右节点的左子元素对比
+    return equal(leftNode.left, rightNode.right) && equal(leftNode.right, rightNode.left)
+  }
+
+  return compare(root)
+}
+
+var r = isSymmetric(new TreeNode(1, new TreeNode(2, null, new TreeNode(4)), new TreeNode(2, null, new TreeNode(4))));
+console.log(r);
+
+/**
+ * method2:层序迭代法，比较每一层的val是否对称，没有子节点的val值用undefined代替
  * @param root
  * @returns {boolean}
  */
@@ -42,39 +78,4 @@ function isSymmetric2(root) {
 
 var r2 = isSymmetric2(new TreeNode(1, new TreeNode(2, null, new TreeNode(4)), new TreeNode(2, null, new TreeNode(4))));
 
-// console.log(r2);
-
-
-/**
- * 递归法
- * @param root
- */
-function isSymmetric3(root) {
-
-  function compare(root) {
-    if (!root) {
-      return false
-    }
-    return equal(root.left, root.right)
-  }
-
-  function equal(leftNode, rightNode) {
-    // 对于本轮对比的两个元素作比对
-    if (leftNode === null && rightNode === null) {
-      return true
-    }
-    if (leftNode === null || rightNode === null) {
-      return false
-    }
-    if (leftNode.val !== rightNode.val) {
-      return false
-    }
-    // 交叉对比：将左节点的左子元素跟右节点的右子元素对比，将左节点的右子元素跟右节点的左子元素对比
-    return equal(leftNode.left, rightNode.right) && equal(leftNode.right, rightNode.left)
-  }
-
-  return compare(root)
-}
-
-var r3 = isSymmetric3(new TreeNode(1, new TreeNode(2, null, new TreeNode(4)), new TreeNode(2, null, new TreeNode(4))));
-console.log(r3);
+console.log(r2);
